b9752b21f00c815b5ab4cfbe84bd955353490fab
Soham Gupta <soham.gupta003@gmail.com>
2024-11-26 00:08:24 +0530

    seo(metadata): for improved SEO and image references

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,25 @@ import { Toaster } from "sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Rate My Dog",
+  metadataBase: new URL("https://dog-rater.vercel.app"),
+  title: {
+    default: "Rate My Dog",
+    template: `%s | Rate My Dog`,
+  },
   description: "Rate dogs and chat with others like cynophilists",
+  openGraph: {
+    title: "Rate My Dog",
+    description: "Rate dogs and chat with others like cynophilists",
+    images: ["/og.png"],
+    url: "https://dog-rater.vercel.app",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Rate My Dog",
+    description: "Rate dogs and chat with others like cynophilists",
+    creator: "@sohamgpt",
+    images: ["/og.png"],
+  },
 };
 
 export default function RootLayout({
